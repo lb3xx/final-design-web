9d593a1e59d6ddfd13c4d2e5c0b3e6d614b0a6fd
3120005201黄俊辉 <3120005201@mail2.gdut.edu.cn>
2024-05-04 18:54:08 +0800

    0504检查点

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -15,7 +15,7 @@
             </span>
         </el-main>
         <el-footer>
-            <el-button type="danger">我要销户！</el-button>
+            <el-button type="danger" @click="dialogAccountVisible = true">我要销户！</el-button>
         </el-footer>
     </el-container>
 
@@ -41,6 +41,17 @@
             </span>
         </template>
     </el-dialog>
+
+    
+    <el-dialog v-model="dialogAccountVisible" title="请确保知悉你正在进行的操作！">
+    身份证号码：<el-input placeholder="请输入身份证信息以供验证"></el-input>
+    <template #footer>
+            <span class="dialog-footer">
+                <el-button @click="dialogAccountVisible = false">取消</el-button>
+                <el-button type="danger">确认销户</el-button>
+            </span>
+        </template>
+    </el-dialog>
 </template>
 
 <script setup>
@@ -53,6 +64,7 @@ const userStore = useUserStore();
 const { userInfo } = storeToRefs(userStore);
 let dialogPhoneVisible = ref(false);
 let dialogPasswordVisible = ref(false);
+let dialogAccountVisible = ref(false);
 let name = userInfo.value.username;
 let phone = userInfo.value.phoneNumber;
 
